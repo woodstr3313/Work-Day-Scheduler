b13f19954b666b459cd76ab28217dde4acbdc15e
Travis Woods <woodstr3313@gmail.com>
2022-02-02 23:25:37 -0500

    Added JS file and js code

---
+++ script.js
@@ -0,0 +1,8 @@
+var date = moment().format("MMMM Do YYYY");
+var currentDay = $("#currentDay");
+currentDay.html(date);
+
+var divEl = $("#button");
+divEl.on("click", ".saveBtn", function(event){
+
+})
